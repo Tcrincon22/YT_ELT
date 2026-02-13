291a9f36b8c6b8badaccfff8c339496df6813de1
Tatis <Tatianawa@gmail.com>
2026-02-13 15:19:41 -0500

    ajustes

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,17 +1,21 @@
 import requests
 import json
 from datetime import date, datetime
-import os 
-from dotenv import load_dotenv
+
+#import os 
+#from dotenv import load_dotenv
+#load_dotenv(dotenv_path="./.env")
+
+from airflow.decorators import task 
+from airflow.models import Variable
 
 
-load_dotenv(dotenv_path="./.env")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
-
+@task
 def get_playlist_id():  
     
     try:
@@ -36,7 +40,7 @@ def get_playlist_id():
         raise e
        
        
-
+@task
 def get_video_ids(playlistId):
     
     video_ids = [] 
@@ -69,12 +73,13 @@ def get_video_ids(playlistId):
         
     except requests.exceptions.RequestException as e:
             raise e
+        
 
 def batch_list(video_ids, batch_size=50):
     for video_id in range(0, len(video_ids), batch_size):
         yield video_ids[video_id:video_id + batch_size]
         
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -109,7 +114,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
- 
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
     
